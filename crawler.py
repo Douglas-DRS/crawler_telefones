7594c8aa4866be809b090bb3c8a02015d16274a8
Douglas-DRS <contato.douglasrafael@proton.me>
2023-11-09 11:48:20 -0300

    :tada: continue of development

--- crawler.py
+++ crawler.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+DOMINIO = 'https://django-anuncios.solyd.com.br'
 URL_AUTOMOVEIS = 'https://django-anuncios.solyd.com.br/automoveis/'
 
 
@@ -26,10 +26,21 @@ def parsing(resposta_html):
     print(error)
 
 
+def encontrar_links(soup):
+  cards_pai = soup.find('div', class_='ui three doubling link cards')
+  cards = cards_pai.find_all('a')
+
+  links= []
+  for card in cards:
+    link = card['href']
+    links.append(link)
+
+  return links
+
+
 resposta = buscar(URL_AUTOMOVEIS)
 if resposta:  
   soup = parsing(resposta)
-
-  cards_pai = soup.find('div', class_='ui three doubling link cards')
-  cards = cards_pai.find_all('a')
-  print(cards)
+  if soup:
+    links = encontrar_links(soup)
+    print(links)
