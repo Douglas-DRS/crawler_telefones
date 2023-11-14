58fdcc3dd62d3309b911de348fdb27ae7d512da5
Douglas-DRS <contato.douglasrafael@proton.me>
2023-11-13 22:34:00 -0300

    :zap: using multithreads

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import re
+import threading
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,67 +7,99 @@ from bs4 import BeautifulSoup
 DOMINIO = 'https://django-anuncios.solyd.com.br'
 URL_AUTOMOVEIS = 'https://django-anuncios.solyd.com.br/automoveis/'
 
+LINKS = []
+TELEFONES = []
+
 
 def requisicao(url):
-		try:
-			resposta = requests.get(url)
-			if resposta.status_code == 200:
-					return resposta.text
-			else:
-					print('Erro ao fazer requisição')
-		except Exception as error:
-				print('Ocorreu um erro inesperado')
-				print(error)
+	try:
+		resposta = requests.get(url)
+		if resposta.status_code == 200:
+			return resposta.text
+		else:
+			print('Erro ao fazer requisição')
+	except Exception as error:
+		print('Ocorreu um erro inesperado')
+		print(error)
 
 
 def parsing(resposta_html):
-		try:
-			soup = BeautifulSoup(resposta_html, 'html.parser')
-			return soup
-		except Exception as error:
-			print('Erro ao fazer parsing do HTML')
-			print(error)
+	try:
+		soup = BeautifulSoup(resposta_html, 'html.parser')
+		return soup
+	except Exception as error:
+		print('Erro ao fazer parsing do HTML')
+		print(error)		
 
 
 def encontrar_links(soup):
+	try:
+		cards_pai = soup.find('div', class_='ui three doubling link cards')
+		cards = cards_pai.find_all('a')
+	except:
+		print("Erro ao encontrar links")
+		return None
+
+	links= []
+	for card in cards:
 		try:
-			cards_pai = soup.find('div', class_='ui three doubling link cards')
-			cards = cards_pai.find_all('a')
+			link = card['href']
+			links.append(link)
 		except:
-			print("Erro ao encontrar links")
-			return None
+			pass
 
-		links= []
-		for card in cards:
-			try:
-				link = card['href']
-				links.append(link)
-			except:
-				pass
-
-		return links
+	return links
 
 
 def encontrar_telefone(soup):
-		try:
-			descricao = soup.find_all('div', class_='sixteen wide column')[2].p.get_text().strip()
-		except:
-			print('Erro ao encontrar descricao')
-			return None
-	  
-		regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", str(descricao))
-		if regex:
-			return regex
-		
+	try:
+		descricao = soup.find_all('div', class_='sixteen wide column')[2].p.get_text().strip()
+	except:
+		print('Erro ao encontrar descricao')
+		return None
+	
+	regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", str(descricao))
+	if regex:
+		return regex
+	
 
-resposta_busca = requisicao(URL_AUTOMOVEIS)
-if resposta_busca:  
+def descobrir_telefones():
+	while True:
+		try:
+			link_anuncio = LINKS.pop(0)
+		except:
+			return None
+
+		resposta_anuncio = requisicao(DOMINIO + link_anuncio)
+
+		if resposta_anuncio:
+			soup_anuncio = parsing(resposta_anuncio)
+			if soup_anuncio:
+				telefones = encontrar_telefone(soup_anuncio)
+				if telefones:
+					for telefone in telefones:
+						print('Telefone encontrado:', telefone)
+						TELEFONES.append(telefone)
+
+
+if __name__ == '__main__':
+	resposta_busca = requisicao(URL_AUTOMOVEIS)
+	if resposta_busca:
 		soup_busca = parsing(resposta_busca)
 		if soup_busca:
-				links = encontrar_links(soup_busca)
-				for link in links:
-						resposta_anuncio = requisicao(DOMINIO + link)
-						if resposta_anuncio:
-								soup_anuncio = parsing(resposta_anuncio)
-								if soup_anuncio:
-										print(encontrar_telefone(soup_anuncio))
+			LINKS = encontrar_links(soup_busca)
+			descobrir_telefones()
+
+			thread_1 = threading.Thread(target=descobrir_telefones)
+			thread_2 = threading.Thread(target=descobrir_telefones)
+			thread_3 = threading.Thread(target=descobrir_telefones)
+
+			thread_1.start()
+			thread_2.start()
+			thread_3.start()
+
+			thread_1.join()
+			thread_2.join()
+			thread_2.join()
+
+			print(TELEFONES)
